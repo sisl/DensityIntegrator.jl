89b496db9cc168e6a34c8373e13abfb895f1909f
Romeo Valentin <romeov@stanford.edu>
2024-02-01 01:26:22 -0800

    Kind of works! But doesn't walk in the direction i was expecting...

--- src/DensityIntegrator.jl
+++ src/DensityIntegrator.jl
@@ -6,16 +6,21 @@ using RecursiveArrayTools
 using Makie, GLMakie
 using RecursiveArrayTools
 using Distributions
+using LazySets
 
 export compute_means_and_dirs, f, run, plot_res
 
 greet() = print("Hello World!")
 
-function compute_means_and_dirs(pts_arr::VectorOfArray; pts_0 = zeros(2))
-    pts = pts_arr.u
+function compute_means_and_dirs(pts::AbstractArray; pts_0 = zeros(2))
     dirs = normalize.(pts .- [pts_0])
     (; dirs)
 end
+# function compute_means_and_dirs(pts_arr::VectorOfArray; pts_0 = zeros(2))
+#     pts = pts_arr.u
+#     dirs = normalize.(pts .- [pts_0])
+#     (; dirs)
+# end
 
 
 function compute_means_and_dirs2(pts; pts_0 = zeros(2))
@@ -39,14 +44,17 @@ function compute_means_and_dirs2(pts; pts_0 = zeros(2))
     (; means, dirs)
 end
 
-function f_(pts, params, t)
-    D = MvNormal(zeros(2), 1.0*I(2))
-    f(D, pts, t)
+function f_(pts_mat, params, t)
+    # D = MvNormal(zeros(2), 1.0*I(2))
+    f(pts_mat, t, params)
 end
-function f(D, pts, t)
-    ax = axes(pts, 2)
+function f(pts_mat, t, params)
+    D = params[:D]
+    pts = eachcol(pts_mat)
+    ax = axes(pts, 1)
+    densities = pdf.([D], pts)
+
     areas = zeros(size(ax))
-    densities = zeros(size(ax))
     for i in ax
         # idx_left  = circshift(ax,  1)[i]
         # idx_right = circshift(ax, -1)[i]
@@ -54,26 +62,29 @@ function f(D, pts, t)
         idx_right= mod1(i+1, length(ax))
         areas[i] = 1/2 * (  norm(pts[i] - pts[idx_left])
                           + norm(pts[i] - pts[idx_right]))
-        densities[i] = pdf(D, pts[i])
     end
-    (; dirs) = compute_means_and_dirs(pts)
+    (; dirs) = compute_means_and_dirs(pts; pts_0=params[:pts_0])
     # @show (densities)
+    # @assert all(abs.(densities .- densities[1]) .< 1e-3) "$(densities .- densities[1])" atol=1e-4
+    # @assert all(abs.(areas .- areas[1]) .< 1e-3) "Areas: $(areas .- areas[1])" atol=1e-4
     # @show [norm(dirs[i]) for i in 1:length(dirs)]
+    # @assert all(norm.(dirs) .≈ 1)
     # @show (areas)
     # @show (densities)
     # @show (1 ./ (areas .* densities))
     # areas = ones(length(pts))
-    slopes = (1 ./ (areas .* densities))
+    slopes = (1 ./ (areas .* max.(densities, 1e-5) .* length(pts)))
+    # slopes .= mean(slopes)
     # @show (slopes)
-    res = VectorOfArray([slopes[i] * dirs[i] for i in 1:length(pts)])
+    res = stack(slopes .* dirs)
     # (; res, densities, areas)
     res
 end
 
 function plot_res(res, D)
     fig = Makie.Figure(); ax = Axis(fig[1,1]; aspect=1);
-    for u in res.u[1:1:end]
-        scatter!(ax, [Point2(u[:, i]) for i in 1:length(u)])
+    for u in res.u
+        scatter!(ax, [Point2(pt) for pt in eachcol(u)])
     end
 
     xs = -10:0.01:10; ys = xs; zs = [pdf(D, [x;y]) for x in xs, y in ys];
@@ -84,18 +95,21 @@ end
 
 
 
-function run()
-    D = MvNormal(zeros(2), 1.0*I(2))
+function run(; pts_0=zeros(2), n=100, p=0.5)
+    # D = MvNormal(zeros(2), 1.0*I(2))
+    D = MvNormal(zeros(2), [1. 0.5; 0.5 1.])
 
     # pts_0 = SVector(0., 0.)
-    pts_0 = zeros(2)
-    n = 10
-    pts = [pts_0 + 1e-6*[cos(t); sin(t)] for t in LinRange(0, 2*pi, n)]
-    pts_arr = VectorOfArray(pts)
+    # pts_0 = zeros(2)
+    # n = 10
+    pts = [pts_0 + 1e-6*[cos(t); sin(t)] for t in LinRange(0, 2*pi, n+1)[1:end-1]]
 
-    prob = ODEProblem((pts,params,t) -> DensityIntegrator.f(D, pts, t), pts_arr, (0.0, 0.01))
+    prob = ODEProblem(DensityIntegrator.f_, stack(pts), (0.0, p), (; pts_0, D))
     res = solve(prob, AutoTsit5(Rosenbrock23()));
-    plot_res(res, D)
+    hull = VPolygon(collect(eachcol(res.u[end])))
+    # plot_res(res, D)
+    @show mean([x ∈ hull for x in eachcol(rand(D, 10_000))])
+    (; res, D)
 end
 
 end # module DensityIntegrator
